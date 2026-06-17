c7d267b08147920e94d25a025a1663e182480479
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-06-17 13:17:57 +0200

    name fix flow

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "aws_connect_contact_flow" "BS-Test-Flow" {
+resource "aws_connect_contact_flow" "BM-Test-Flow" {
   instance_id  = var.instance_id
-  name         = "BS-Test-IaC-Flow"
+  name         = "BM-Test-IaC-Flow"
   description  = "Test Contact Flow made by IaC with file upload"
   type         = "CONTACT_FLOW"
   filename     = "contact_flow.json"
   content_hash = filebase64sha256("contact_flow.json")
   tags = {
-    "Name"        = "BS-Test-IaC-Flow",
+    "Name"        = "BM-Test-IaC-Flow",
     "Application" = "Terraform",
     "Method"      = "Create"
   }
