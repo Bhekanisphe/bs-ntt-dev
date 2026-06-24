72d18a6a228d4b188191b6e5be822e336a6c63db
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-06-24 16:56:05 +0200

    fix dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -32,13 +32,15 @@ resource "aws_dynamodb_table_item" "Test-Case-Items" {
         "queue_id"                 = { "S" = each.value.queue_id },
         "type"                     = { "S" = each.value.type },
         "welcome_text"             = { "S" = each.value.welcome_text },
-        "menu_levels"              = { "M" = for key, value in each.value.menu_levels : key => {
+        "menu_levels"              = { "M" = { for key, value in each.value.menu_levels : key => {
             "M" = {
                 "identifier"  = { "S" = value.identifier },
                 "message"     = { "S" = value.message },
                 "user_action" = { "S" = value.user_action },
                 "next"        = { "S" = value.next }
             }
+        }
+        
         }}
     })
 
