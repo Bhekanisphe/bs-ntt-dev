8f7224fe4b31a54db3fca5475c29fd9b0b3b8ade
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-06-24 13:37:59 +0200

    add dynamodb invocations

--- dynamodb.tf
+++ dynamodb.tf
@@ -3,13 +3,46 @@ resource "aws_dynamodb_table" "BS-Automated-Testing-Table" {
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "flow-name:testing-option"
 
-  dynamic "attribute" {
-    for_each = var.attributes
     attribute {
-      name = attribute.value.name
-      type = attribute.value.type
+        name = "flow-name:testing-option"
+        type = "S"
     }
-
+    attribute {
+        name = "caller_number"
+        type = "S"
+    }
+    attribute {
+        name = "description"
+        type = "S"
+    }
+    attribute {
+        name = "flow_id"
+        type = "S"
+    }
+    attribute {
+        name = "hho_id"
+        type = "S"
+    }
+    attribute {
+        name = "hoo_result"
+        type = "S"
+    }
+    attribute {
+        name = "queue_id"
+        type = "S"
+    }
+    attribute {
+        name = "type"
+        type = "S"
+    }
+    attribute {
+        name = "welcome_text"
+        type = "S"
+    }
+    attribute {
+        name = "menu_levels"
+        type = "M"
+    }
   # Enable stream — sends change events to Lambda
   stream_enabled   = true
   stream_view_type = "NEW_AND_OLD_IMAGES" # OPTIONS: KEYS_ONLY | NEW_IMAGE | OLD_IMAGE | NEW_AND_OLD_IMAGES
