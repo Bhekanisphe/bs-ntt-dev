e017a023b1d9e5b8c1cf9c1d429f00cc5b4a6945
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-06-24 13:44:49 +0200

    add dynamodb invocations fix

--- dynamodb.tf
+++ dynamodb.tf
@@ -2,46 +2,10 @@ resource "aws_dynamodb_table" "BS-Automated-Testing-Table" {
   name         = "bs-automated-testing-iac"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "flow-name:testing-option"
-
+    // Only primary key is required for a DynamoDB table, but you can add additional attributes if needed
     attribute {
         name = "flow-name:testing-option"
         type = "S"
-    }
-    attribute {
-        name = "caller_number"
-        type = "S"
-    }
-    attribute {
-        name = "description"
-        type = "S"
-    }
-    attribute {
-        name = "flow_id"
-        type = "S"
-    }
-    attribute {
-        name = "hho_id"
-        type = "S"
-    }
-    attribute {
-        name = "hoo_result"
-        type = "S"
-    }
-    attribute {
-        name = "queue_id"
-        type = "S"
-    }
-    attribute {
-        name = "type"
-        type = "S"
-    }
-    attribute {
-        name = "welcome_text"
-        type = "S"
-    }
-    attribute {
-        name = "menu_levels"
-        type = "M"
     }
   # Enable stream — sends change events to Lambda
   stream_enabled   = true
