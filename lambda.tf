964b21ccae3b1f35dfb089015459fec300b17ecc
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-06-24 11:04:39 +0200

    fix lambda

--- lambda.tf
+++ lambda.tf
@@ -20,8 +20,8 @@ resource "aws_iam_role" "lambda_role" {
 # Package the Lambda function code
 data "archive_file" "lambda_file" {
   type        = "zip"
-  source_file = "${path.module}/../lambda_functions/auto_test_lambda.py"
-  output_path = "${path.module}/../lambda_functions/function.zip"
+  source_file = "${path.root}/../lambda_functions/auto_test_lambda.py"
+  output_path = "${path.root}/../lambda_functions/function.zip"
 }
 
 # Lambda function
@@ -29,7 +29,7 @@ resource "aws_lambda_function" "bs-automated-testing" {
   filename      = data.archive_file.lambda_file.output_path
   function_name = "bs-automated-testing-iac"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "lambda_handler"
+  handler       = "auto_test_lambda.lambda_handler"
   source_code_hash = filebase64sha256(data.archive_file.lambda_file.output_path)
   //code_sha256   = data.archive_file.lambda_file.output_base64sha256
 
