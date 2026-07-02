41b4cdf7e1b5b0f8de15540868fc2a7745beb03c
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-07-02 16:36:43 +0200

    add dyanmodb for error handling

--- lambda.tf
+++ lambda.tf
@@ -84,15 +84,7 @@ resource "aws_lambda_function" "bs-automated-testing_error_handling" {
   }
 }
 
-resource "aws_lambda_event_source_mapping" "lambda_dynamodb_trigger" {
-  event_source_arn  = aws_dynamodb_table.BS-Automated-Testing-Table.stream_arn
-  function_name     = aws_lambda_function.bs-automated-testing.arn
-  starting_position = "LATEST"
 
-  tags = {
-    Name = "dynamodb-stream-mapping"
-  }
-}
 
 resource "aws_lambda_event_source_mapping" "lambda_dynamodb_trigger_error_handling" {
   event_source_arn  = aws_dynamodb_table.BS-Automated-Testing-Table.stream_arn
